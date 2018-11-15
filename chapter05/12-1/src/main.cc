9e90cddd0f73cb32cbfb88e534adfa16e6ea2382
MaxEntroy <likang@tju.edu.cn>
2018-11-15 10:06:26 +0800

    12-1: 修改main.cc

--- chapter05/12-1/src/main.cc
+++ chapter05/12-1/src/main.cc
@@ -1,8 +1,22 @@
 #include "inc/replace_index.h"
+#include <vector>
 
 int main(void) {
-    const std::string input_str("The quick brown fox");
-    std::string output_str;
-    replace_index(input_str, &output_str);
+    const std::string input_str1("The quick brown fox");
+    const std::string input_str2("jumped over the fence");
+
+    std::vector<std::string> input_sentence;
+    input_sentece.push_back(input_str1);
+    input_sentece.push_back(input_str2);
+
+    std::vector<std::string> output_sentence;
+
+    replace_index(input_sentence, output_sentence);
+
+    int sz = output_sentence.size();
+    for(int i = 0; i < sz; ++i) {
+        std::cout << output_sentence[i] << std::endl;
+    }
+
     return 0;
 }
