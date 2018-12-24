7d6b2930586949121f408b92548be04338a3b39c
MaxEntroy <likang@tju.edu.cn>
2018-12-24 15:06:53 +0800

    bug: chapter06/16: 增加构造时成绩初始化

--- chapter06/16/inc/student_info.h
+++ chapter06/16/inc/student_info.h
@@ -9,6 +9,8 @@ struct StudentInfo {
     double midterm_score;
     double final_score;
     std::vector<double> homework_score;
+
+    StudentInfo() : midterm_score(0.0), final_score(0.0) {}
 };
 
 bool cmp_student(const StudentInfo& lhs, const StudentInfo& rhs);
